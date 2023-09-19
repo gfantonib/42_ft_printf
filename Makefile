eef5e01c5434cc661a009a456ebd175748a77367
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2023-09-19 20:30:08 -0300

    melvin

--- Makefile
+++ Makefile
@@ -1,3 +1,42 @@
+#NAME = libftprintf.a
+#
+#LIBFT_NAME = libft/libft.a
+#LIBFT_PATH = libft
+#CFLAGS = -Wall -Wextra -Werror -g
+#IFLAGS = -Iinc -Ilibft/inc
+#CC = cc
+#MK = mkdir -p
+#
+#SOURCES = \
+#	src/ft_printf.c
+#
+#OBJECTS_DIR = objs
+#OBJECTS = $(addprefix $(OBJECTS_DIR)/, $(SOURCES:%.c=%.o))
+#
+#all: $(LIBFT_NAME) $(NAME)
+#
+#
+#$(OBJECTS_DIR)/%.o: %.c
+#	$(MK) $(@D)
+#	$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
+#
+#$(LIBFT_NAME):
+#	make -C $(LIBFT_PATH)
+#
+#$(NAME): $(OBJECTS)
+#	ar rcsT $(NAME) $(OBJECTS) $(LIBFT_NAME) $?
+#	
+#clean:
+#	make clean -C $(LIBFT_PATH)
+#	rm -rf $(OBJECTS_DIR)
+#
+#fclean: clean
+#	make fclean -C $(LIBFT_PATH)
+#	rm -f $(NAME)
+#
+#re: fclean all
+#
+#.PHONY: all clean fclean re
 NAME = libftprintf.a
 
 LIBFT_NAME = libft/libft.a
@@ -6,9 +45,10 @@ CFLAGS = -Wall -Wextra -Werror -g
 IFLAGS = -Iinc -Ilibft/inc
 CC = gcc
 MK = mkdir -p
+SRC_PATH = src/
 
 SOURCES = \
-	src/ft_printf.c
+    ft_printf.c
 
 OBJECTS_DIR = objs
 OBJECTS = $(addprefix $(OBJECTS_DIR)/, $(SOURCES:%.c=%.o))
@@ -16,16 +56,17 @@ OBJECTS = $(addprefix $(OBJECTS_DIR)/, $(SOURCES:%.c=%.o))
 all: $(LIBFT_NAME) $(NAME)
 
 
-$(OBJECTS_DIR)/%.o: %.c
+$(OBJECTS_DIR)/%.o: $(SRC_PATH)%.c
 	$(MK) $(@D)
 	$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
 
 $(LIBFT_NAME):
 	make -C $(LIBFT_PATH)
+	cp $(LIBFT_NAME) $(NAME)
 
 $(NAME): $(OBJECTS)
-	ar rcsT $(NAME) $(OBJECTS) $(LIBFT_NAME) $?
-	
+	ar rcs $(NAME) $(OBJECTS)
+    
 clean:
 	make clean -C $(LIBFT_PATH)
 	rm -rf $(OBJECTS_DIR)
--- main.c
+++ main.c
@@ -4,34 +4,63 @@
 int	main(void)
 {
 	char	*str = "F. Scott Fitzgerald";
-	int	nbr_p = 1969;
-	int	nbr_n = -1969;
+	void	*str_v = "void Scott Fitzgerald";
+	int	nbr_p = 2147483647;
+	int	nbr_n = -2147483648;
 
-	ft_printf("------------------------------\n");
+	printf("\n");
 	ft_printf("MANDATORY PART(c, s, p, d, i, u, x, X, %%)\n");
+	printf("\n");
+	ft_printf("---------------------------------------------\n");
+
 	ft_printf("c: %c\n", *str);
-	ft_printf("s: %s\n", str);
-	ft_printf("p: %p\n", str);
-	ft_printf("d(p): %+d | d(n): %+d\n", nbr_p, nbr_n);
-	ft_printf("i(p): % i | i(n): %i \n", nbr_p, nbr_n);
-	ft_printf("u(p): %u | u(n): %u\n", nbr_p, nbr_n);
-	ft_printf("x(p): %#x | x(n): %x\n", nbr_p, nbr_n);
-	ft_printf("X(p): %#X | X(n): %X\n", nbr_p, nbr_n);
-	ft_printf("%%: %%\n");
-	ft_printf("------------------------------\n");
-	printf("MANDATORY PART(c, s, p, d, i, u, x, X, %%)\n");
 	printf("c: %c\n", *str);
+	
+	printf("\n");
+
+	ft_printf("s: %s\n", str);
 	printf("s: %s\n", str);
-	printf("p: %p\n", str);
-	printf("d(p): %+d | d(n): %+d\n", nbr_p, nbr_n);
-	printf("i(p): % i | i(n): %i \n", nbr_p, nbr_n);
-	printf("u(p): %u | u(n): %u\n", nbr_p, nbr_n);
-	printf("x(p): %#x | x(n): %x\n", nbr_p, nbr_n);
-	printf("X(p): %#X | X(n): %X\n", nbr_p, nbr_n);
-	printf("%%: %%\n");
+
+	printf("\n");
+
+	ft_printf("p: %p\n", str_v);
+	printf("p: %p\n", str_v);
+
+	printf("\n");
+
+	ft_printf("d: %d | %d\n", nbr_p, nbr_n);
+	printf("d: %d | %d\n", nbr_p, nbr_n);
+
+	printf("\n");
+
+	ft_printf("i: %i | %i\n", nbr_p, nbr_n);
+	printf("i: %i | %i\n", nbr_p, nbr_n);
+
+	printf("\n");
+
+	ft_printf("u: %u | %u\n", nbr_p, nbr_n);
+	printf("u: %u | %u\n", nbr_p, nbr_n);
+
+	printf("\n");
+
+	ft_printf("x: %x | %x\n", nbr_p, nbr_n);
+	printf("x: %x | %x\n", nbr_p, nbr_n);
+
+	printf("\n");
+
+	ft_printf("X: %X | %X\n", nbr_p, nbr_n);
+	printf("X: %X | %X\n", nbr_p, nbr_n);
+
+	printf("\n");
+
+	printf("%d\n", ft_printf("%%: %%\n"));
+	printf("%d\n", printf("%%: %%\n"));
+
+	printf("\n");
+	
 	printf("%d\n", printf(0));
 	printf("%d\n", ft_printf(0));
-	printf("------------------------------\n");
+	ft_printf("---------------------------------------------\n");
 	return (0);
 }
 
